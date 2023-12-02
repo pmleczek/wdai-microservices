717952932c9569bce781d456f03b6999f13a65e8
hyper <patrykmleczek.pm@gmail.com>
2023-12-02 21:53:55 +0100

    Add HelloController controller

---
+++ spring/src/main/java/com/pmleczek/microservice/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.pmleczek.microservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(defaultValue = "World", name = "name") String name) {
+        return "Hello, " + name + "!";
+    }
+}
